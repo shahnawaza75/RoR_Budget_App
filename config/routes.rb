804cbead0c2ca0c4a9c19c8dcc3df88a21f0caed
Shahnawaz Ali <shahnawaza75@gmail.com>
2022-10-16 01:29:50 +0530

    add open letter route

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'users#index'
 
-  mount LetterOpenerWeb::Engine, at: '/letter_opener'
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
